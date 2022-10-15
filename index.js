45a93ec44d963f3ab415c7a0ecace5058c620d75
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-10-15 16:07:52 +0700

    Using While and Promise

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const readline = require('readline')
-
-const cmd = readline.createInterface({
+const cmd = require('readline').createInterface({
 	input: process.stdin, 
 	output: process.stdout,
 })
@@ -8,32 +6,6 @@ const cmd = readline.createInterface({
 const currency = new Intl.NumberFormat("id-ID", {
 	currency: "IDR",
 	style: "currency"
-})
-
-console.log('Hitung Biaya Ongkos')
-
-const app = () => {
-	cmd.question('Jarak tempuh: ', jarak => {
-		let { fee, service } = calculate(jarak)
-
-		console.log('Total ongkos kirim = ' + currency.format(fee))
-		console.log('Biaya Layanan = ' + currency.format(service))
-	
-		cmd.question('Ulang(y/n): ', answer => {
-			if (answer.toLowerCase() == 'n') {
-				cmd.close()
-			} else {
-				app()
-			}
-		})
-	})
-}
-
-app()
-
-cmd.on('close', () => {
-	console.log('Bye!')
-	process.exit(0)
 })
 
 const calculate = (distance) => {
@@ -50,3 +22,41 @@ const calculate = (distance) => {
 		}
 	}
 }
+
+const menu = () => {
+  return new Promise((resolve, reject) => {
+		cmd.question('Jarak tempuh: ', jarak => {
+			let { fee, service } = calculate(jarak)
+			console.log('Total ongkos kirim = ' + currency.format(fee))
+			console.log('Biaya Layanan = ' + currency.format(service))
+			
+			cmd.question('Ulang(y/n): ', answer => {
+				if (answer.toLowerCase() == 'n') {
+					reject(false)
+				} else {
+					resolve(true)
+				}
+			})
+    })
+  })
+}
+
+const app = async () => {
+	let repeat = true
+
+	while (repeat) {
+		cmd.setPrompt('Hitung Biaya Ongkos')
+    await menu().catch(err => {
+			repeat = false
+    })
+	}
+
+	cmd.close()
+}
+
+cmd.on('close', () => {
+	console.log('Bye!')
+	process.exit(0)
+})
+
+app()
